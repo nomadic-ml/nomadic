a9e171ce455fec433763815ec94477620af5d5be
Mustafa Bal <5262061+mstfbl@users.noreply.github.com>
2024-12-24 01:09:13 -0500

    Nit add

--- cookbooks/Advanced_RAG_Optimization_Retrieval_&_Inferencing_Stages.py
+++ cookbooks/Advanced_RAG_Optimization_Retrieval_&_Inferencing_Stages.py
@@ -18,6 +18,8 @@ import pandas as pd
 pd.set_option('display.max_colwidth', None)
 import json
 
+
+# Ensure Nomadic API Key exists
 from nomadic.client import NomadicClient, ClientOptions
 if "NOMADIC_API_KEY" in os.environ:
     nomadic_client = NomadicClient(ClientOptions(api_key=os.environ["NOMADIC_API_KEY"], base_url="http://204.236.146.45"))
@@ -42,7 +44,7 @@ overlap = tune.choice([25])
 similarity_threshold = tune.choice([50])
 top_k =  tune.choice([1, 2])
 max_tokens = tune.choice([100, 200])
-model_name = tune.choice(["gpt-3.5-turbo", "gpt-4o"])
+model_name = tune.choice(["gpt-4o-mini", "gpt-4o"])
 embedding_model = tune.choice(["text-embedding-ada-002", "text-embedding-curie-001"])
 retrieval_strategy = tune.choice(["sentence-window", "auto-merging"])
 
@@ -50,7 +52,8 @@ retrieval_strategy = tune.choice(["sentence-window", "auto-merging"])
 # Obtain RAG inputs
 docs, eval_qs, ref_response_strs = obtain_rag_inputs(pdf_url=pdf_url, eval_json=eval_json)
 
-# Run retrieval experiment
+# If retrieval experiment already exists in Workspace, retrieve it.
+# Otherwise, create a new retrieval experiment and run it.
 experiment_retrieval = Experiment(
     param_fn=run_retrieval_pipeline,
     params = {"top_k", "model_name", "retrieval_strategy", "embedding_model"},
@@ -59,10 +62,10 @@ experiment_retrieval = Experiment(
         "eval_qs": eval_qs[:10],
         "ref_response_strs": ref_response_strs[:10],
     },
+    client_id="1d9254f4-4fac-4dc3-8b2d-2a1e544d1658",
     enable_logging=False,
 )
-
-# After the retrieval is done
+# After the retrieval is done...
 retrieval_results = experiment_retrieval.run(param_dict={
         "top_k": top_k,
         "model_name": model_name,
@@ -73,12 +76,13 @@ save_run_results(retrieval_results, "run_results.json")
 
 retrieval_results.run_results
 
-# Load the saved results and get the best run result
+# ...Load the saved results and get the best run result
 loaded_results = load_run_results("run_results.json")
 best_run_result = get_best_run_result(loaded_results)
 best_retrieval_results = best_run_result['metadata'].get("best_retrieval_results", [])
 
-# Run inference experiment
+# If inference experiment already exists in Workspace, retrieve it.
+# Otherwise, create a new retrieval experiment and run it.
 experiment_inference = Experiment(
     param_fn=run_inference_pipeline,
     params={"temperature","model_name", "max_tokens", "reranking_model", "similarity_threshold"},
@@ -87,6 +91,7 @@ experiment_inference = Experiment(
         "ref_response_strs": ref_response_strs[:10],  # Make sure this matches the number of queries used in retrieval,
         "enable_logging": True
     },
+    client_id="3c1f7815-882e-4b9c-b7ed-556205396963",
     enable_logging=False,
 )
 
